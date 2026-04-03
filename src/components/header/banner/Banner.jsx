75ca59d91408c8c51b654ef7cafbefd67373f5e4
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-04-03 16:16:00 +0200

    build: banner button with gradient effect

--- src/components/header/banner/Banner.jsx
+++ src/components/header/banner/Banner.jsx
@@ -21,7 +21,7 @@ const Banner = () => {
                     </p>
                     <div className='flex justify-center items-center gap-1'>
                         <CustomButton text={"Explore Products"}></CustomButton>
-                        <button className="btn btn-outline rounded-2xl"><CiPlay1 />
+                        <button className="btn border rounded-2xl border-[#4F39F6] bg-linear-to-r from-[#4F39F6] to-[#627382] bg-clip-text text-transparent"><CiPlay1 className='text-[#4F39F6]'/>
                             Watch Demo</button>
                     </div>
                 </div>

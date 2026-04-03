647255b19bc45d3a120b9a86a660d28d3b2b98a4
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-04-03 14:56:18 +0200

    build: customs buttons are responsive with props

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import CustomButton from '../../ui/CustomButton';
 const Navbar = ({ navLinksPromise }) => {
     const navLinks = use(navLinksPromise);
     return (
-        <div className='flex justify-between items-center mx-auto container'>
+        <div className='sticky z-100 top-0 flex justify-between items-center mx-auto container'>
             <div className="md:hidden dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
@@ -19,16 +19,17 @@ const Navbar = ({ navLinksPromise }) => {
                     {navLinks.map(link => <Link key={link.id} link={link}></Link>)}
                 </ul>
             </div>
-            <h2 className='bg-linear-to-r from-[#4F39F6] to-[#627382] bg-clip-text inline-block text-transparent font-bold text-2xl md:text-4xl'>DigiTools</h2>
+            <h2 className='bg-linear-to-r from-[#4F39F6] to-[#627382] bg-clip-text inline-block text-transparent font-bold text-2xl md:text-4xl pb-1'>DigiTools</h2>
             <ul className='hidden md:flex justify-between items-center list-none gap-2'>
                 {navLinks.map(link => link.name !== 'Login' && <Link key={link.id} link={link}></Link>)}
             </ul>
-            <div className='hidden md:flex justify-between items-center gap-1'>
+            <div className='flex justify-between items-center'>
                 <button className='btn btn-ghost'><IoIosCart /></button>
-                <LoginButton></LoginButton>
-                <CustomButton></CustomButton>
+                <div className='hidden md:flex justify-between items-center gap-1'>
+                    <LoginButton></LoginButton>
+                    <CustomButton text={"Get Started"}></CustomButton>
+                </div>
             </div>
-            <button className='btn btn-ghost md:hidden'><IoIosCart /></button>
         </div>
     );
 };

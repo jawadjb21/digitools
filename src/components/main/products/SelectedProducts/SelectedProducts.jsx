c878c83793524dd3498150f3e9e26dd1ca5a0509
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-04-05 11:46:18 +0200

    build: checkout button clears cart

--- src/components/main/products/SelectedProducts/SelectedProducts.jsx
+++ src/components/main/products/SelectedProducts/SelectedProducts.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 import SelectedProductCard from '../../../ui/SelectedProductCard';
+import CustomButton from '../../../ui/CustomButton';
 
 
 const SelectedProducts = ({ selectedProducts, setSelectedProducts }) => {
@@ -7,7 +9,10 @@ const SelectedProducts = ({ selectedProducts, setSelectedProducts }) => {
     let total = 0;
     total = prices.map(price => total += Number(price));
     total = total.reverse();
-    console.log(total[0]);
+    const handleCheckout = () => {
+        setSelectedProducts([]);
+        toast.success("Cart has been cleared!")
+    }
     return (
         <div className='flex flex-col justify-center items-start text-left gap-y-3 md:gap-5 place-items-center mx-auto md:max-w-5xl px-5 mt-5'>
             <h6 className='font-bold text-xl'>Your Cart</h6>
@@ -17,6 +22,7 @@ const SelectedProducts = ({ selectedProducts, setSelectedProducts }) => {
             {
                 selectedProducts.length > 0 && <div className='flex justify-between items-center w-full'><span className='font-bold text-2xl'>Total:</span><span className='font-bold text-2xl'>{`$${total[0].toFixed(2)}`}</span></div>
             }
+            <CustomButton text={"Checkout"} forBuying={true} onClick={() => handleCheckout()} disabled={selectedProducts.length === 0}></CustomButton>
         </div>
 
     );

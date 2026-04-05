850248c85187a0ca8babf6f0a9f8a63d6883f92d
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-04-05 15:46:35 +0200

    fix: checkout button conditional

--- src/components/main/products/SelectedProducts/SelectedProducts.jsx
+++ src/components/main/products/SelectedProducts/SelectedProducts.jsx
@@ -9,10 +9,16 @@ const SelectedProducts = ({ selectedProducts, setSelectedProducts }) => {
     let total = 0;
     total = prices.map(price => total += Number(price));
     total = total.reverse();
+    let isAnySelected = false;
+    if(selectedProducts.length > 0){
+        isAnySelected = true;
+    }
+
     const handleCheckout = () => {
         setSelectedProducts([]);
         toast.success("Cart has been cleared!")
     }
+
     return (
         <div className='flex flex-col justify-center items-start text-left gap-y-3 md:gap-5 place-items-center mx-auto md:max-w-5xl px-5 mt-5'>
             <h6 className='font-bold text-xl'>Your Cart</h6>
@@ -22,7 +28,9 @@ const SelectedProducts = ({ selectedProducts, setSelectedProducts }) => {
             {
                 selectedProducts.length > 0 && <div className='flex justify-between items-center w-full'><span className='font-bold text-2xl'>Total:</span><span className='font-bold text-2xl'>{`$${total[0].toFixed(2)}`}</span></div>
             }
-            <CustomButton text={"Checkout"} forBuying={true} onClick={() => handleCheckout()} disabled={selectedProducts.length === 0}></CustomButton>
+            {
+                isAnySelected && <CustomButton text={"Checkout"} forBuying={true} onClick={() => handleCheckout()} disabled={selectedProducts.length === 0}></CustomButton>
+            }
         </div>
 
     );
